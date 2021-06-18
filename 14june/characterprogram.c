b6281d4ce9aeef4f9db2cb6615734d38277d1671
Manish Pandey <85880847+Manish9616@users.noreply.github.com>
2021-06-19 01:50:57 +0530

    Add files via upload

---
+++ 14june/characterprogram.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+        char ch1,ch2,ch3;
+        ch1 = 65;
+        ch2='b';
+        ch3='3';
+        printf("ch1= %c\n",ch1);
+        printf("ch1= %c\n",ch2);
+printf("ch1= %c\n",ch3);
+printf("ch1= %d\n",ch1);
+printf("ch1= %d\n",ch2);
+printf("ch1= %d\n",ch3);
+}   
