93b79cb3ba47df9a09b1e1c11f99c8d1d52fbf00
Manish Pandey <85880847+Manish9616@users.noreply.github.com>
2021-06-19 02:08:47 +0530

    Create Assignment3.c

---
+++ 16june/Assignment3.c
@@ -0,0 +1,33 @@
+/ conversion using recursion
+#include <stdio.h>
+ 
+// Decimal to binary conversion
+// using recursion
+
+int find(int decimal_number)
+{
+
+    if (decimal_number == 0) 
+
+        return 0; 
+
+    else
+
+        return (decimal_number % 2 + 10 * 
+
+                find(decimal_number / 2));
+}
+ 
+// Driver code 
+
+int main()
+{
+
+    int decimal_number = 10;
+
+    printf("%d", find(decimal_number));
+
+    return 0;
+}
+
+Output : 1010
